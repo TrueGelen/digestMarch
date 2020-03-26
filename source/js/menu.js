9966196c481bbb724b7ab2290e3eec9d111c6913
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-03-26 14:10:43 +0300

    test mob menu

--- source/js/menu.js
+++ source/js/menu.js
@@ -5,29 +5,49 @@ window.addEventListener('load', () => {
   const menuItems = menu.querySelectorAll('.menu__items a')
   const close = menu.querySelector('.menu__close')
 
+  //--------kanishev menu---------
+  const bgForMobMenu = document.querySelector('#bgForMobMenu')
   function showMenu() {
-    burger.style.opacity = 0
-
-    menu.classList.remove('hideMenu')
-    menu.classList.add('showMenu')
-
-    close.classList.remove('hideClose')
-    close.classList.add('showClose')
+    console.log(1)
+    if (!bgForMobMenu.classList.contains('bgForMobMenu_opened')) {
+      bgForMobMenu.classList.add('bgForMobMenu_opened')
+      menu.style.display = 'block'
+    } else
+      hideMenu()
   }
 
   function hideMenu() {
-    burger.style.opacity = 1
-
-    menu.classList.remove('showMenu')
-    menu.classList.add('hideMenu')
-
-    close.classList.remove('showClose')
-    close.classList.add('hideClose')
+    bgForMobMenu.classList.remove('bgForMobMenu_opened')
+    menu.style.display = 'none'
   }
 
   burger.addEventListener('click', showMenu)
 
-  close.addEventListener('click', hideMenu)
+  //-----old menu----
+
+  // function showMenu() {
+  //   burger.style.opacity = 0
+
+  //   menu.classList.remove('hideMenu')
+  //   menu.classList.add('showMenu')
+
+  //   close.classList.remove('hideClose')
+  //   close.classList.add('showClose')
+  // }
+
+  // function hideMenu() {
+  //   burger.style.opacity = 1
+
+  //   menu.classList.remove('showMenu')
+  //   menu.classList.add('hideMenu')
+
+  //   close.classList.remove('showClose')
+  //   close.classList.add('hideClose')
+  // }
+
+  // burger.addEventListener('click', showMenu)
+
+  // close.addEventListener('click', hideMenu)
 
   for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener('click', () => {
@@ -35,40 +55,40 @@ window.addEventListener('load', () => {
     })
   }
 
-  //------------------------------------------
+  //------------------------menu1-----------
 
-  const nectarinLogo = document.querySelector('#logonectarin')
+  // const nectarinLogo = document.querySelector('#logonectarin')
 
-  function buildLogoAndBurger() {
-    if (window.innerWidth > 768) {
-      burger.classList.remove('hideMenuOnMobile')
-      nectarinLogo.classList.remove('hideMenuOnMobile')
-    }
-  }
+  // function buildLogoAndBurger() {
+  //   if (window.innerWidth > 768) {
+  //     burger.classList.remove('hideMenuOnMobile')
+  //     nectarinLogo.classList.remove('hideMenuOnMobile')
+  //   }
+  // }
 
-  let curPosY = 0
-  function menuWatcher() {
-    if (window.innerWidth <= 768) {
-      if (curPosY < window.pageYOffset) {
-        burger.classList.add('hideMenuOnMobile')
-        nectarinLogo.classList.add('hideMenuOnMobile')
-        curPosY = window.pageYOffset
-      } else {
-        burger.classList.remove('hideMenuOnMobile')
-        nectarinLogo.classList.remove('hideMenuOnMobile')
-        curPosY = window.pageYOffset
-      }
-    }
-  }
+  // let curPosY = 0
+  // function menuWatcher() {
+  //   if (window.innerWidth <= 768) {
+  //     if (curPosY < window.pageYOffset) {
+  //       burger.classList.add('hideMenuOnMobile')
+  //       nectarinLogo.classList.add('hideMenuOnMobile')
+  //       curPosY = window.pageYOffset
+  //     } else {
+  //       burger.classList.remove('hideMenuOnMobile')
+  //       nectarinLogo.classList.remove('hideMenuOnMobile')
+  //       curPosY = window.pageYOffset
+  //     }
+  //   }
+  // }
 
-  // buildLogoAndBurger()
-  window.addEventListener('resize', () => {
-    // buildLogoAndBurger()
-    buildLogoAndBurger()
-  })
+  // // buildLogoAndBurger()
+  // window.addEventListener('resize', () => {
+  //   // buildLogoAndBurger()
+  //   buildLogoAndBurger()
+  // })
 
-  window.addEventListener('scroll', () => {
-    menuWatcher()
-  })
+  // window.addEventListener('scroll', () => {
+  //   menuWatcher()
+  // })
 
 })
